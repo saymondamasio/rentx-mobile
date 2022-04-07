ce5d7ab5ec5947e5b4ffcf893e3a52aec55d46d3
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-07 08:42:53 -0300

    feat: add Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import { RFValue } from 'react-native-responsive-fontsize'
 import Logo from '../../assets/logo.svg'
-import { Container, Header } from './styles'
+import { Container, Header, HeaderContent, TotalCars } from './styles'
 
 export function Home() {
   return (
     <Container>
       <StatusBar translucent backgroundColor="transparent" style="light" />
       <Header>
-        <Logo />
+        <HeaderContent>
+          <Logo height={RFValue(12)} width={RFValue(108)} />
+
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
       </Header>
     </Container>
   )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -12,4 +12,19 @@ export const Header = styled.View`
   height: ${RFValue(113)}px;
 
   background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+
+  padding: 32px 24px;
+`
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `
