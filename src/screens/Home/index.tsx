d2073603fa72f9ca8de619ec1635542655a1d16b
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-07 08:26:31 -0300

    chore: add react native svg

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,15 @@
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import Logo from '../../assets/logo.svg'
 import { Container, Header } from './styles'
 
 export function Home() {
   return (
     <Container>
-      <Header></Header>
+      <StatusBar translucent backgroundColor="transparent" style="light" />
+      <Header>
+        <Logo />
+      </Header>
     </Container>
   )
 }
