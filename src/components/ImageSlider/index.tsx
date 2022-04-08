9176ea3294d8d8e8cc11379fa89eb32fc63f0e52
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-08 12:45:37 -0300

    feat: add ImageSlider component

---
+++ src/components/ImageSlider/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  CarImage,
+  CarImageWrapper,
+  Container,
+  ImageIndex,
+  ImageIndexes,
+} from './styles'
+
+interface Props {
+  imagesUrl: string[]
+}
+
+export function ImageSlider({ imagesUrl }: Props) {
+  return (
+    <Container>
+      <ImageIndexes>
+        <ImageIndex active={true} />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+      </ImageIndexes>
+
+      <CarImageWrapper>
+        <CarImage source={{ uri: imagesUrl[0] }} resizeMode="contain" />
+      </CarImageWrapper>
+    </Container>
+  )
+}
