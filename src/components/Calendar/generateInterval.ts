6a60b7c7538914678d705982cd9fb66f4d856c84
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-10 21:27:39 -0300

    feat: create generateInterval function

---
+++ src/components/Calendar/generateInterval.ts
@@ -0,0 +1,31 @@
+import { eachDayOfInterval, format } from 'date-fns'
+import { DateData } from 'react-native-calendars'
+import { MarkedDatesType } from '.'
+import theme from '../../styles/theme'
+import { getPlatformDate } from '../../utils/getPlatformDate'
+
+export function generateInterval(start: DateData, end: DateData) {
+  let interval: MarkedDatesType = {}
+
+  eachDayOfInterval({
+    start: new Date(start.timestamp),
+    end: new Date(end.timestamp),
+  }).forEach(item => {
+    const date = format(getPlatformDate(item), 'yyyy-MM-dd')
+    interval = {
+      ...interval,
+      [date]: {
+        color:
+          start.dateString === date || end.dateString === date
+            ? theme.colors.main
+            : theme.colors.main_light,
+        textColor:
+          start.dateString === date || end.dateString === date
+            ? theme.colors.main_light
+            : theme.colors.main,
+      },
+    }
+  })
+
+  return interval
+}
