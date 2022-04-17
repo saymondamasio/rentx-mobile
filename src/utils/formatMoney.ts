ab347d77b4bc5a453f3ecd1220ae208b03a86f84
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-17 18:09:22 -0300

    fix: intl format

--- src/utils/formatMoney.ts
+++ src/utils/formatMoney.ts
@@ -1,6 +1,9 @@
+import Intl from 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+
 export function formatMoney(amount: number) {
-  return amount.toLocaleString('pt-BR', {
+  return new Intl.NumberFormat('pt-BR', {
     currency: 'BRL',
     style: 'currency',
-  })
+  }).format(amount)
 }
