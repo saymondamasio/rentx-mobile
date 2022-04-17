7332cf54f355699ff50d11eb2dc91ffb9accf90e
Saymon Damásio <saymon.damasio95@gmail.com>
2022-04-16 22:17:41 -0300

    feat: add updateUser function

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -22,6 +22,7 @@ interface AuthContextData {
   user: User
   signIn: (credentials: SignCredentials) => Promise<void>
   signOut: () => Promise<void>
+  updateUser: (user: User) => Promise<void>
 }
 
 interface Props {
@@ -90,8 +91,26 @@ export function AuthProvider({ children }: Props) {
     }
   }
 
+  async function updateUser(user: User) {
+    try {
+      const userCollection = database.get<ModelUser>('users')
+      await database.write(async () => {
+        const userSelected = await userCollection.find(data.id)
+        await userSelected.update(userData => {
+          userData.name = user.name
+          userData.driver_license = user.driver_license
+          userData.avatar = user.avatar
+        })
+      })
+
+      setData(user)
+    } catch (error: any) {
+      throw new Error(error)
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ signIn, signOut, user: data }}>
+    <AuthContext.Provider value={{ signIn, signOut, updateUser, user: data }}>
       {children}
     </AuthContext.Provider>
   )
